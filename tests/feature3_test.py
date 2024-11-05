9d38251265807ec9452f4910a4bae2a570def140
Lucia Pursals <luciapursals@gmail.com>
2024-11-05 23:49:04 +0100

    add tests for feature 3

---
+++ tests/feature3_test.py
@@ -0,0 +1,7 @@
+from feature3 import feature3, feature3_final
+
+def test_feature3():
+    assert feature3() == "feature 3 implementation"
+
+def test_feature3_final():
+    assert feature3_final() == "feature 3 completed and finalized"
