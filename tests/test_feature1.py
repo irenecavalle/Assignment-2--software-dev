b02935eff0a50a0875b889afcd98346e256e83b4
Lucia Tortajada <luciatortajada@Lucias-MacBook-Air-3.local>
2024-11-05 23:32:02 +0100

    Add tests for Feature 1

---
+++ tests/test_feature1.py
@@ -0,0 +1,8 @@
+from feature1 import feature1, feature1_final
+
+def test_feature1():
+    assert feature1() == "Feature 1 start"
+
+def test_feature1_final():
+    assert feature1_final() == "Feature 1 completed"
+
